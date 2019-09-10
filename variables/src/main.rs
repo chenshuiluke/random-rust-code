bf23456eb12e52c6bfe13a6dd5bf06ee60acc9b5
Luke Chen Shui <chenshuiluke@gmail.com>
2019-09-09 21:48:01 -0500

    Added notes from today's reading of the variables chapter.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -14,4 +14,14 @@ fn main() {
     let spaces = spaces.len();
 
     println!("The number of spaces is: {}", spaces);
+
+    //Rust's integer types go from 8 bits to 64 bits, ie i8 to u64
+    //They can also be signed or unsigned, ie i8 or u8
+
+
+    //Rust has two floating point types: f32 and f64
+
+    //Rust has the bool type to represent true and false
+
+    //Rust has characters which are unicode
 }
